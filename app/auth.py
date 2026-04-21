d923b81689a9348e0c914eb73adcccb96c5dc7c4
Yun NaJa <151639314+Kasidet1@users.noreply.github.com>
2026-04-21 16:13:34 +0700

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -14,11 +14,11 @@ security = HTTPBearer()
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])
+    return pwd_context.hash(password)
 
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password[:72], hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def create_access_token(data: dict):
@@ -39,6 +39,7 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
     
+
 def require_role(required_role: str):
     def role_checker(user = Depends(get_current_user)):
         if user["role"] != required_role:
