dc5bc1be2843760788a0d5b16ac75d3ef0481108
Yun NaJa <151639314+Kasidet1@users.noreply.github.com>
2026-05-18 18:03:49 +0700

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
-from passlib.context import CryptContext
 
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
@@ -8,46 +7,96 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 
-# แก้ตรงนี้
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
-
 security = HTTPBearer()
 
 
+# ======================
+# PASSWORD
+# ======================
+
 def hash_password(password: str):
-    return pwd_context.hash(password)
+
+    # ไม่ hash
+    return password
 
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(
+    plain_password,
+    saved_password
+):
+
+    # เทียบตรงๆ
+    return plain_password == saved_password
 
 
-def create_access_token(data: dict):
+# ======================
+# JWT
+# ======================
+
+def create_access_token(
+    data: dict
+):
+
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=60)
-    to_encode.update({"exp": expire})
 
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    expire = datetime.utcnow() + timedelta(
+        minutes=60
+    )
+
+    to_encode.update({
+        "exp": expire
+    })
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
 
 
-def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+
     token = credentials.credentials
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
         return payload
 
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
 
 
-def require_role(required_role: str):
-    def role_checker(user = Depends(get_current_user)):
+# ======================
+# ROLE
+# ======================
+
+def require_role(
+    required_role: str
+):
+
+    def role_checker(
+        user=Depends(get_current_user)
+    ):
+
         if user["role"] != required_role:
-            raise HTTPException(status_code=403, detail="Permission denied")
+
+            raise HTTPException(
+                status_code=403,
+                detail="Permission denied"
+            )
+
         return user
 
     return role_checker
